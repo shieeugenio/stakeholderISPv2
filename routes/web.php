d9e89bfc3a24a543d2e462427addedd73f14476f
Shiela Eugenio <shielamaeeugenio@rocketmail.com>
2016-12-02 17:25:06 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -150,8 +150,3 @@ Route::get('logout', array('uses' => 'HomeController@logout'));
 //registration[ren]
 Route::get('registration', 'RegistrationController@index');
 Route::resource('register', 'RegistrationController@register');
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
