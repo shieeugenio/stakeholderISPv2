11bd6c9f5d7621eb6d0dd29c492683cc7fa97b4e
Ren William Buluran <Ren William Buluran>
2017-01-09 20:05:38 +0800

    ren

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -12,25 +12,40 @@ use Redirect;
 class RegistrationController extends Controller
 {
     public function index(){
-    	return View('transaction.registration');
+        return View('transaction.registration');
     }
 
     public function register(Request $req){
-    	$validate = users::where('email', '=', $req->username)->first();
-    	if ($validate === null) {
-    		$user = new users;
-	        $user->name = $req->name;
-	        $user->email = $req->username;
-	        $user->created_at = date('Y-m-d H:i:s');;
-	        $user->password = bcrypt($req->password);
-	        $user->save();
-	        $message = "registration complete";
-	        return Redirect::to('login')->with('message',$message);
+        $validate = users::where('email', '=', $req->username)->first();
+        if ($validate === null) {
+            $user = new users;
+            $user->name = $req->name;
+            $user->email = $req->username;
+            $user->type = $req->type;
+            $user->status = 0;
+            $user->created_at = date('Y-m-d H:i:s');
+            $user->password = bcrypt($req->password);
+            $user->save();
+            $message = "registration complete";
+            return Redirect::to('login')->with('message',$message);
 
-    	}else{
-	    	$message = "username already exist!";
-	        return Redirect::to('registration')->with('message',$message);
+        }else{
+            $message = "username already exist!";
+            return Redirect::to('registration')->with('message',$message);
         }
     }
+
+    public function approvalSuccess(Request $req){
+            $user = users::find($req->id);
+            $user->status = 1;
+            $user->save();
+    }
+
+    public function approvalCancel(Request $req){
+            $user = users::find($req->id);
+            $user->status = 2;
+            $user->save();
+    }
+    
 
 }
