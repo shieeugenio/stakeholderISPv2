e4c40b62f03db7c1262955c36fdcacbe414a7c35
JoanneJoyce <dasigjoanne@yahoo.com>
2017-01-09 14:37:19 +0800

    unitoffices 1 and 2

--- app/Http/Controllers/PoliceOfficesController.php
+++ app/Http/Controllers/PoliceOfficesController.php
@@ -11,35 +11,11 @@ class PoliceOfficesController extends Controller
     public function index(){
     	$office = App\Models\unit_offices::all();
 
-        $staffdesc = $this->getstaffdesc($office);
-
-		return view('maintenancetable/policeoffice_table')->with('offices', $office)
-                                                          ->with('staffdesc', $staffdesc);
+		return view('maintenancetable/policeoffice_table')->with('offices', $office);
 
         //return $staffdesc;
 	}
-
-    public function getstaffdesc($office) {
-
-        $desclist = array();
-        foreach($office as $key => $items) {
-
-            if($items->policestaff == 0) {
-                $description = "D-Staff";
-            } else if($items->policestaff == 1) {
-                $description = "P-Staff";
-
-
-            }//if($items->policestaff == 0) {
-
-            $desclist = array_pad($desclist, sizeof($desclist) + 1, $description);
-
-        }//foreach($office as $key => $items) {
-
-        return $desclist;
-
-    }//public getstaffdesc() {
-
+    
     public function confirmOffice(Request $request){
     	if(isset($_POST['addbtn'])){
     		return $this->insert($request);
@@ -50,12 +26,8 @@ class PoliceOfficesController extends Controller
     public function add(Request $request){
         if(isset($_POST['submit'])){
             $office = new App\Models\unit_offices;
-            $office->policeofficecode = $request->input('code');
-            $office->officename = $request->input('name');
-            $office->policestaff = $request->input('staff');
-    	    $office->desc = $request->input('desc');
-    	    $office->police_address = $request->input('add');
-    	    $office->contactno = $request->input('contact');
+            $office->UnitOfficeName = $request->input('name');
+            $office->UnitOfficeHasField = $request->input('hassec');
 
     	    $office->save();
         }
@@ -71,12 +43,8 @@ class PoliceOfficesController extends Controller
     	if(isset($_POST['submit'])){
     		$id = App\Models\unit_offices::find($request->policeID);
 
-    		$id->policeofficecode = $request->code;
-    		$id->officename = $request->name;
-            $id->policestaff = $request->staff;
-    		$id->desc = $request->desc;
-    		$id->police_address = $request->add;
-    		$id->contactno = $request->contact;
+    		$id->UnitOfficeName = $request->name;
+            $id->UnitOfficeHasField = $request->hassec;
 
     		$id->save();
     	}
