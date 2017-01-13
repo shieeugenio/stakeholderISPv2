a1d83e8e794c884b8eeb8d5cbad65a75f2a76e9e
JoanneJoyce <dasigjoanne@yahoo.com>
2017-01-13 08:42:17 +0800

    db ulit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,14 +6,11 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+   
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) 
+        {
             $table->increments('id');
             $table->string('name');
             $table->tinyInteger('status');
